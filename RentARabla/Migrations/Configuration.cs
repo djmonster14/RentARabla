e6a80c2971b9a1c221fedae414063ee2e7ac02d3
sorests <sorests@yahoo.com>
2016-11-12 19:07:10 +0200

    Added Seeds

--- RentARabla/Migrations/Configuration.cs
+++ RentARabla/Migrations/Configuration.cs
@@ -17,6 +17,8 @@ namespace RentARabla.Migrations
         protected override void Seed(Contexts.RentARablaDBContext context)
         {
             SeedAdmin(context);
+            SeedCars(context);
+            SeedClients(context);
         }
 
         private void SeedAdmin(Contexts.RentARablaDBContext context)
@@ -42,5 +44,118 @@ namespace RentARabla.Migrations
                     Person = person
                 });
         }
+        private void SeedCars(Contexts.RentARablaDBContext context) {
+            var car = context.Cars.FirstOrDefault();
+            if (car != null)
+                return;
+            var price1 = new Price
+            {
+                Currency = Currency.EUR,
+                PriceValue = 35
+            };
+            var car1 = new Car
+            {
+
+                PricePerDay = price1,
+                ManufactureDate = Convert.ToDateTime("01/08/2016"),
+                FuelType = FuelType.Diesel,
+                Type = CarType.Jeep,
+                Brand = CarBrand.Dacia,
+                Model = CarModel.Duster
+            };
+
+            var price2 = new Price
+            {
+                Currency = Currency.EUR,
+                PriceValue = 40
+            };
+            var car2 = new Car
+            {
+
+                PricePerDay = price2,
+                ManufactureDate = Convert.ToDateTime("01/08/2010"),
+                FuelType = FuelType.Electric,
+                Type = CarType.Luxury,
+                Brand = CarBrand.Tesla,
+                Model = CarModel.Roadster
+            };
+
+            var price3 = new Price
+            {
+                Currency = Currency.EUR,
+                PriceValue = 45
+            };
+            var car3 = new Car
+            {
+
+                PricePerDay = price3,
+                ManufactureDate = Convert.ToDateTime("05/08/2012"),
+                FuelType = FuelType.Petrol,
+                Type = CarType.Jeep,
+                Brand = CarBrand.Volkswagen,
+                Model = CarModel.Tuareg
+            };
+
+            context.Cars.Add(car1);
+            context.Cars.Add(car2);
+            context.Cars.Add(car3);
+            //context.SaveChanges();
+        }
+
+        private void SeedClients(Contexts.RentARablaDBContext context)
+        {
+            var client = context.Administrators.FirstOrDefault();
+            if (client != null)
+                return;
+
+            var address1 = new Address
+            {
+                Street = "Mihai Viteazul",
+                StreetNumber = 4,
+                City = "Cluj-Napoca",
+                Country = "Romania"
+            };
+
+            var person1 = new Person
+            {
+                FirstName = "Matei",
+                LastName = "Serafim",
+                UserName = "mserafim",
+                Password = "1234",
+                Email = "mserafim@yahoo.com",
+                Phone = "0744012012"
+            };
+
+            context.Clients.Add(
+                new Client
+                {
+                    Age = 43,
+                    NationalId = "1881101234432",
+                    Person = person1,
+                    Address = address1
+                });
+
+            var person2 = new Person
+            {
+                FirstName = "Sorin",
+                LastName = "Decebal",
+                UserName = "sdecebal",
+                Password = "1234",
+                Email = "sdecebal@yahoo.com",
+                Phone = "0743123123"
+            };
+
+            context.Clients.Add(
+                new Client
+                {
+                    Age = 47,
+                    NationalId = "2841101342433",
+                    Person = person2,
+                    Address = address1
+                });
+
+            //context.SaveChanges();
+        }
+
     }
 }
